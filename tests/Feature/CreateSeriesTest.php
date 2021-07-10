5f0ac45396c01b7fa486f171477959dbd8a8fde2
ANrajin <an.rajin@gmail.com>
2021-07-10 16:50:10 +0600

    admin test added

--- tests/Feature/CreateSeriesTest.php
+++ tests/Feature/CreateSeriesTest.php
@@ -9,6 +9,7 @@ use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use App\Models\User;
 
 class CreateSeriesTest extends TestCase
 {
@@ -18,6 +19,8 @@ class CreateSeriesTest extends TestCase
     public function test_a_user_can_create_a_series()
     {
         $this->withoutExceptionHandling();
+
+        $this->loginAdmin();
 
         Storage::fake(config('filesystems.default'));
 
@@ -40,6 +43,8 @@ class CreateSeriesTest extends TestCase
 
     public function test_a_series_must_be_created_with_title_and_description()
     {
+        $this->loginAdmin();
+
         $this->post('/admin/series', [
             'image' => UploadedFile::fake()->image('new-img.png')
         ])->assertSessionHasErrors(['title', 'description']);
@@ -47,6 +52,8 @@ class CreateSeriesTest extends TestCase
 
     public function test_a_series_must_be_created_with_image()
     {
+        $this->loginAdmin();
+
         $this->post('/admin/series', [
             'title' => 'this is a title',
             'description' => 'this is the description',
@@ -55,6 +62,8 @@ class CreateSeriesTest extends TestCase
 
     public function test_a_series_must_be_created_with_image_that_must_be_image()
     {
+        $this->loginAdmin();
+
         $this->post('/admin/series', [
             'title' => 'this is a title',
             'description' => 'this is the description',
@@ -65,10 +74,21 @@ class CreateSeriesTest extends TestCase
 
     public function test_a_series_must_be_created_with_image_that_must_be_150_200()
     {
+        $this->loginAdmin();
+
         $this->post('/admin/series', [
             'title' => 'this is a title',
             'description' => 'this is the description',
             'image' => UploadedFile::fake()->image('new-img.png', 150, 100)
         ])->assertSessionHasErrors('image');
+    }
+
+    public function test_a_user_cannot_access_admin_route()
+    {
+        $this->withExceptionHandling();
+
+        $this->actingAs(User::factory()->make());
+
+        $this->post('/admin/series')->assertSessionHas('error', 'You are not authorized to perform this action.');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function loginAdmin()
+    {
+        $user = User::factory()->make();
+
+        Config::push('laravue.admins', $user->email);
+
+        $this->actingAs($user);
+    }
 }
